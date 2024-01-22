65c706f4e5b48689aae785ca33dc20ef5714e54c
dvc-77 <neilohene@gmail.com>
2024-01-22 00:12:18 +0000

    feat: main.py which contains the main class and requirements.txt for libraries used

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+class TimeTableExtractor:
+    def __init__(self, timetable):
+        self.timetable = pd.DataFrame(timetable)
+
+    def extract(self, criteria):
+        # Implementation goes here
+    
+        pass
