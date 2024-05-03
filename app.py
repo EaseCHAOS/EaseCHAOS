7d9d50ea891d06e82f4cb7653520838e90be09b7
N E I L  O H E N E <54632119+dvc-77@users.noreply.github.com>
2024-05-03 22:17:22 +0000

    feat: new frontend

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = FastAPI()
 
 app_router = APIRouter(prefix="/api/v1")
 
-@app_router.get("/")
+@app.get("/api/v1")
 def root():
     return {"Hello": "World"}
 
-app.include_router(app_router)
+app.include_router(router=app_router)
 app.include_router(timetable_router, prefix="/api/v1")
